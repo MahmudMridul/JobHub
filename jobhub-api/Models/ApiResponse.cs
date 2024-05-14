365a8cd117ad92122dd6c199c894886f50d86827
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-05-14 08:47:05 +0600

    Add ApiResponse

---
+++ jobhub-api/Models/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace jobhub_api.Models
+{
+    public class ApiResponse
+    {
+        public object? Data { get; set; } = null;
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess {  get; set; }
+        public bool IsError { get; set; }
+        public bool IsInfo { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
