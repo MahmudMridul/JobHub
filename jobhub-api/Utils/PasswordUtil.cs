0f9a5a3ce6842340b7fdcd2551e5fdd32e35cc68
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-06-04 15:33:24 +0600

    Add Utils for functions

---
+++ jobhub-api/Utils/PasswordUtil.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace jobhub_api.Utils
+{
+    internal class PasswordUtil
+    {
+        internal static byte[] GenerateSalt(int size = 16)
+        {
+            byte[] salt = new byte[size];
+            RandomNumberGenerator.Fill(salt);
+            return salt;
+        }
+
+        internal static string HashPassword(string password, byte[] salt)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                byte[] saltedPassword = new byte[salt.Length + passwordBytes.Length];
+
+                Buffer.BlockCopy(passwordBytes, 0, saltedPassword, 0, passwordBytes.Length);
+                Buffer.BlockCopy(salt, 0, saltedPassword, passwordBytes.Length, salt.Length);
+
+                byte[] hashedBytes = sha256.ComputeHash(saltedPassword);
+
+                byte[] hashedPasswordWithSalt = new byte[hashedBytes.Length + salt.Length];
+                Buffer.BlockCopy(salt, 0, hashedPasswordWithSalt, 0, salt.Length);
+                Buffer.BlockCopy(hashedBytes, 0, hashedPasswordWithSalt, salt.Length, hashedBytes.Length);
+
+                return Convert.ToBase64String(hashedPasswordWithSalt);
+            }
+        }
+    }
+}
---
+++ jobhub-api/Utils/TokenUtil.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace jobhub_api.Utils
+{
+    internal class TokenUtil
+    {
+        internal static string GetToken(string userName, string secretKey)
+        {
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(secretKey);
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, userName)
+                    }
+                ),
+                Expires = DateTime.UtcNow.AddMinutes(20),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            SecurityToken token = handler.CreateToken(descriptor);
+            return handler.WriteToken(token);
+        }
+    }
+}
