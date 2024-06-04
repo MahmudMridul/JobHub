34aa82d7c1b0ac1beb8afacab0fa3cd30554bd58
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-06-04 12:42:53 +0600

    Update user

--- jobhub-api/Models/User.cs
+++ jobhub-api/Models/User.cs
@@ -1,11 +1,15 @@
-﻿namespace jobhub_api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace jobhub_api.Models
 {
     public class User
     {
-        public required int ID { get; set; }
+        [Key]
+        public int ID { get; set; }
         public required string Name { get; set; }
         public required string Email { get; set; }
-        public required string Password { get; set; }
+        public required string PasswordHash { get; set; }
+        public required byte[] Salt { get; set; }
         //Relation with Role
         public required Role Role { get; set; }
     }
