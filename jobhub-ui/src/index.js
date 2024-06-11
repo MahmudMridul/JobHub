17580c07e21415f8c6fe66e2e352391471c9f2ea
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-06-11 18:06:23 +0600

    Add fake login and login utils

--- jobhub-ui/src/index.js
+++ jobhub-ui/src/index.js
@@ -4,12 +4,29 @@ import './index.css';
 import App from './App';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './theme';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <App />
+   },
+   {
+      path: "/home",
+      element: <Home />
+   }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <React.StrictMode>
-      <ThemeProvider theme={theme}>
-         <App />
-      </ThemeProvider>
+      <Provider store={store}>
+         <ThemeProvider theme={theme}>
+            <RouterProvider router={router} />
+         </ThemeProvider>
+      </Provider>
    </React.StrictMode>
 );
